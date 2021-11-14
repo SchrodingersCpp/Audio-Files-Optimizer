0b3241a236949bd5a49ba00ed2c47fd38216c4ce
Schrödinger's Cat <18612775+SchrodingersCpp@users.noreply.github.com>
2021-11-14 18:30:41 +0200

    Update audioConverter.py

--- audioConverter.py
+++ audioConverter.py
@@ -5,13 +5,35 @@ import csv
 import subprocess
 
 def readFileListData(infoFile: str) -> \
-    typing.Tuple[typing.List[str], typing.List[int], typing.List[str],
-                 typing.List[str], typing.List[str]]:
+    typing.Tuple[typing.List[str], typing.List[str], typing.List[int],
+                 typing.List[str], typing.List[str], typing.List[str]]:
     """
     TODO
     """
     
-    pass
+    fullName = []
+    existFileName = []
+    kbps = []
+    newFileName = []
+    title = []
+    artist = []
+    
+    # read the content of a CSV file
+    with open(infoFile, 'r') as csvfile:
+        reader = csv.reader(csvfile, dialect='excel')
+        next(reader)                     # skip the header line
+        for row in reader:
+            if len(row) > 8:             # files to process
+                processedName = row[8]   # gey a new file name value
+                if processedName != '':  # check if a new file name exists
+                    fullName.append(row[0])
+                    existFileName.append(row[1])
+                    kbps.append(row[4])
+                    newFileName.append(row[8])
+                    title.append(row[9])
+                    artist.append(row[10])
+    
+    return fullName, existFileName, kbps, newFileName, title, artist
 
 def convertAudioFiles(infoFile: str, outFolder: str, outkbps: int) -> None:
     """
@@ -35,10 +57,16 @@ def convertAudioFiles(infoFile: str, outFolder: str, outkbps: int) -> None:
     ffmpeg = 'ffmpeg'                   # converter command
     sysFunctions.cmdInstalled(ffmpeg)   # check if command is installed
     
+    fullName, existFileName, kbps, newFileName, title, artist = \
+        readFileListData(infoFile)
+    
+    # TODO
+    # write a CSV output with converted files info
+    
     return 0
 
-infoFile = r'/home/linux/Documents/TESTOUT/out_2021-11-12_08-55-43.csv'
+infoFile = r'/home/linux/Documents/TESTOUT/out_2021-11-14_18-09-31.csv'
 outFolder = r'/home/linux/Documents/TESTOUT'
-outkbps = 128.0
+outkbps = 128
 
 convertAudioFiles(infoFile, outFolder, outkbps)
