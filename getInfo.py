43416fcc51d2486f9d8436cb1fa5999d6aaebee6
SchrodingersCpp <18612775+SchrodingersCpp@users.noreply.github.com>
2024-03-01 19:09:41 +0200

    Tab size changed

--- getInfo.py
+++ getInfo.py
@@ -2,37 +2,37 @@ import fileFunctions as ff
 import sysFunctions as sf
 
 def getInfo(folderIn: str, folderOut: str) -> int:
-    """
-    Gets file info and writes it to a file.
+  """
+  Gets file info and writes it to a file.
 
-    folderIn : str
-        Folder being processed.
+  folderIn : str
+    Folder being processed.
 
-    folderOut: str
-        Folder to which an output file is written.
+  folderOut: str
+    Folder to which an output file is written.
 
-    Returns 0.
-    """
+  Returns 0.
+  """
 
-    sf.limitTracebackInfo(0) # limit traceback info
-    ff.dirCheck(folderIn)    # check "folderIn" correctness
-    ff.dirCheck(folderOut)   # check "folderOut" correctness
+  sf.limitTracebackInfo(0) # limit traceback info
+  ff.dirCheck(folderIn)    # check "folderIn" correctness
+  ff.dirCheck(folderOut)   # check "folderOut" correctness
 
-    fileName, fullName = ff.listFiles(folderIn, True) # list files in directory
+  fileName, fullName = ff.listFiles(folderIn, True) # list files in directory
 
-    bitrateType, kbps, title, artist = \
-        ff.getMetadata(fullName) # get metadata from filelist
+  bitrateType, kbps, title, artist = \
+      ff.getMetadata(fullName) # get metadata from filelist
 
-    fileSize   = ff.getFileSize(fullName)      # get the size of the files
-    extensions = ff.getFileExtension(fileName) # get files extensions
+  fileSize   = ff.getFileSize(fullName)      # get the size of the files
+  extensions = ff.getFileExtension(fileName) # get files extensions
 
-    ff.writeData(folderIn, fullName, fileName, extensions, bitrateType, kbps,
-                 title, artist, fileSize, folderOut) # write data to a file
+  ff.writeData(folderIn, fullName, fileName, extensions, bitrateType, kbps,
+               title, artist, fileSize, folderOut) # write data to a file
 
-    return 0
+  return 0
 
 if __name__ == '__main__':
-    folder = r'/home/linux/Documents/TEST'
-    out    = r'/home/linux/Documents/TESTOUT'
-    print(getInfo(folder, out))
+  folder = r'/home/linux/Downloads/mtest2/'
+  out    = r'/home/linux/Downloads/convert/'
+  print(getInfo(folder, out))
 
--- sysFunctions.py
+++ sysFunctions.py
@@ -5,71 +5,71 @@ import sys
 import psutil
 
 def limitTracebackInfo(depth: int=0) -> None:
-    """
-    Limits traceback information.
+  """
+  Limits traceback information.
 
-    depth : int
-        Depth of traceback information output. Defaults to 0 (no output).
+  depth : int
+    Depth of traceback information output. Defaults to 0 (no output).
 
-    Returns None.
-    """
+  Returns None.
+  """
 
-    # check "depth" argument type provided
-    if not isinstance(depth, int):
-        raise TypeError('"depth" must be an integer ' +
-                        f'({type(depth)} was provided)!')
+  # check "depth" argument type provided
+  if not isinstance(depth, int):
+    raise TypeError('"depth" must be an integer ' +
+                    f'({type(depth)} was provided)!')
 
-    # check "depth" argument value provided
-    if depth < 0:
-        raise ValueError('"depth" value must be positive or 0 ' +
-                         f'("{depth}" was provided)!')
+  # check "depth" argument value provided
+  if depth < 0:
+    raise ValueError('"depth" value must be positive or 0 ' +
+                     f'("{depth}" was provided)!')
 
-    # limit traceback information
-    sys.tracebacklimit = depth
+  # limit traceback information
+  sys.tracebacklimit = depth
 
-    return None
+  return None
 
 def cmdInstalled(cmd: str) -> None:
-    """
-    Checks whether the specified command is installed.
+  """
+  Checks whether the specified command is installed.
 
-    cmd : str
-        Command to be checked.
+  cmd : str
+    Command to be checked.
 
-    Returns None.
-    """
+  Returns None.
+  """
 
-    # check "cmd" argument type provided
-    if not isinstance(cmd, str):
-        raise TypeError('"cmd" must be a string ' +
-                        f'("{type(cmd)}" was provided)!')
+  # check "cmd" argument type provided
+  if not isinstance(cmd, str):
+    raise TypeError('"cmd" must be a string ' +
+                    f'("{type(cmd)}" was provided)!')
 
-    # check whether cmd command is installed
-    cmdInstalled = shutil.which(cmd)
-    if cmdInstalled is None:
-        raise Exception(f'"{cmd}" command is not installed!')
+  # check whether cmd command is installed
+  cmdInstalled = shutil.which(cmd)
+  if cmdInstalled is None:
+    raise Exception(f'"{cmd}" command is not installed!')
 
-    return None
+  return None
 
 def nPhysicalCores() -> int:
-    """
-    Get number of CPU physical cores.
+  """
+  Get number of CPU physical cores.
 
-    Returns:
-        Number of CPU physical cores : str.
-    """
-    return psutil.cpu_count(logical=False)
+  Returns:
+    Number of CPU physical cores : str.
+  """
+  return psutil.cpu_count(logical=False)
 
 def getTimeStamp() -> str:
-    """
-    Get current timestamp.
+  """
+  Get current timestamp.
 
-    Returns:
-        Current timestamp : str.
-    """
+  Returns:
+    Current timestamp : str.
+  """
 
-    timestamp = dt.datetime.now() # get a timestamp for a filename
-    timestamp = timestamp.strftime('_%Y-%m-%d_%H-%M-%S')
+  timestamp = dt.datetime.now() # get a timestamp for a filename
+  timestamp = timestamp.strftime('_%Y-%m-%d_%H-%M-%S')
 
-    return timestamp
+  return timestamp
 
